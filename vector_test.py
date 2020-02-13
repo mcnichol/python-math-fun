100dcd4b9036f3a9f7023e9bc9a11c3e884e435c
mcnichol <mcnichol.m@gmail.com>
2020-02-13 15:43:58 -0600

    Pytest in requirements dev, init vector tests

---
+++ vector_test.py
@@ -0,0 +1,12 @@
+from vector import *
+
+
+def test_vector_adds():
+    vector_1 = Vector([1, 2, 3])
+    vector_2 = Vector([1, 2, 3])
+
+    actual = vector_1 + vector_2
+
+    expected = Vector([2, 4, 6])
+
+    assert actual == expected
